0547c8e97cff6a0432a5bf5563f3791536259d3d
unknown <ak-rakib>
2022-10-11 11:28:35 -0700

    rechart added

--- src/Components/Statistic/Statistic.js
+++ src/Components/Statistic/Statistic.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip
+} from "recharts";
 
 const Statistic = () => {
+    const data = useLoaderData().data;
+    console.log(data)
     return (
         <div>
-            <h1>This is Statistic</h1>
+            <h1 bg='warning'>Mark Analysis Chart</h1>
+            <AreaChart
+      width={400}
+      height={400}
+      data={data}
+      margin={{
+        top: 10,
+        right: 30,
+        left: 0,
+        bottom: 0
+      }}
+    
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Area type="monotone" dataKey="total" stroke="#8884d8" fill="#8884d8" />
+    </AreaChart>
         </div>
     );
 };
