9d6f0c6cb1353feac8e64ea090fb29bd7858f06c
unknown <ak-rakib>
2022-10-11 22:40:10 -0700

    statistic chart reset

--- src/Components/Statistic/Statistic.js
+++ src/Components/Statistic/Statistic.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip
-} from "recharts";
 import './Statistic.css';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistic = () => {
     const data = useLoaderData().data;
@@ -16,24 +9,27 @@ const Statistic = () => {
     return (
         <div>
             <h1 className='statistic-header'>Mark Analysis Chart</h1>
-            <AreaChart
-      width={400}
-      height={400}
-      data={data}
-      margin={{
-        top: 10,
-        right: 30,
-        left: 0,
-        bottom: 0
-      }}
-    
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Area type="monotone" dataKey="total" stroke="#8884d8" fill="#8884d8" />
-    </AreaChart>
+      <ResponsiveContainer width="95%" height={400}>
+        <LineChart className='chart'
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
         </div>
     );
 };
